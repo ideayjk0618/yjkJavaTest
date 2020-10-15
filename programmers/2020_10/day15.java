3f2caa6dd0f9f21e8d27a5544e0d9f6dc3f9fd72
YJK <70570855+ideayjk0618@users.noreply.github.com>
2020-10-15 13:32:49 +0900

    Update day15.java

--- programmers/2020_10/day15.java
+++ programmers/2020_10/day15.java
@@ -1,1 +1,27 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        
+        if (n < 1 || n > Math.pow(50, 13)) {
+            return answer;
+        } else {
+            // 제곱근 값을 문자열로 변환.
+            String strSqrt = String.valueOf(Math.sqrt(n));
+            // 소숫점 위치
+            int getPoint = strSqrt.indexOf(".");
+            // 소숫점 앞까지만 출력.
+            strSqrt = strSqrt.substring(0, getPoint);
+            long longSqrt = Long.parseLong(strSqrt);
+            if (n == Math.pow(longSqrt, 2)) { // 여기는 
+                longSqrt++;
+                // 실수 -> 정수로 형변환
+                answer = (long)Math.pow(longSqrt, 2);
+            } else {
+                answer = -1;
+            }
+            
+        }
+        System.out.print(144 == 144.0);
+        return answer;
+    }
 
