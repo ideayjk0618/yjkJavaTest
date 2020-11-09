23aa5c4b88b015dddb661f195a83ee39f47eb14d
YJK <70570855+ideayjk0618@users.noreply.github.com>
2020-11-09 20:00:54 +0900

    조이스틱(3차)
    10,11 성공했으나 새롭게 4,5,7,8에 문제가 발생.

---
+++ programmers/2020_11/day07.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int solution(String name) {
+        int answer = 0;
+
+        // 이동횟수
+        int number = 0;
+        int right = 0; // 오른쪽 이동
+        int left = 0; // 왼쪽 이동
+
+        // 이름 길이 만큼 반복.
+        for (int i = 0; i < name.length(); i++) {
+            int getAbc = (int) name.charAt(i);
+            if (65 < getAbc) {
+            	if (i != 0) {
+            		right++;
+            	}
+            	int up = getAbc - 65; // 커서 위
+            	int down = 91 - getAbc; // 커서 아래
+            	if (up <= down) {
+            		number += up;
+            	} else {
+            		number += down;
+            	}
+            } else if (65 == getAbc) {
+                int mid = name.length() / 2;
+                if (i < mid) {
+                    left = i+1;
+                } else {
+                    left = right;
+                }
+            }
+        }
+        // System.out.print(number);
+        // System.out.print(right +":"+left);
+        answer = number + left + right;
+
+        return answer;
+    }
+}
