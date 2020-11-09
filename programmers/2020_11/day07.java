6a46b705620a030b5028896be416accd0015a197
YJK <70570855+ideayjk0618@users.noreply.github.com>
2020-11-09 23:39:31 +0900

    조이스틱 (최종) _그리디(탐욕)알고리즘
    -> 그리디 알고리즘 : 현재 상태에 제일 좋아 보이는것을 택한다. 
    이 문제에서는 최소 이동하는것이 제일 좋아 보이는것으로 인식한것.
    문제 설명
    조이스틱으로 알파벳 이름을 완성하세요. 맨 처음엔 A로만 이루어져 있습니다.
    ex) 완성해야 하는 이름이 세 글자면 AAA, 네 글자면 AAAA
    
    조이스틱을 각 방향으로 움직이면 아래와 같습니다.
    
    ▲ - 다음 알파벳
    ▼ - 이전 알파벳 (A에서 아래쪽으로 이동하면 Z로)
    ◀ - 커서를 왼쪽으로 이동 (첫 번째 위치에서 왼쪽으로 이동하면 마지막 문자에 커서)
    ▶ - 커서를 오른쪽으로 이동
    예를 들어 아래의 방법으로 JAZ를 만들 수 있습니다.
    
    - 첫 번째 위치에서 조이스틱을 위로 9번 조작하여 J를 완성합니다.
    - 조이스틱을 왼쪽으로 1번 조작하여 커서를 마지막 문자 위치로 이동시킵니다.
    - 마지막 위치에서 조이스틱을 아래로 1번 조작하여 Z를 완성합니다.
    따라서 11번 이동시켜 "JAZ"를 만들 수 있고, 이때가 최소 이동입니다.
    만들고자 하는 이름 name이 매개변수로 주어질 때, 이름에 대해 조이스틱 조작 횟수의 최솟값을 return 하도록 solution 함수를 만드세요.
    
    제한 사항
    name은 알파벳 대문자로만 이루어져 있습니다.
    name의 길이는 1 이상 20 이하입니다.
    입출력 예
    name	return
    JEROEN	56
    JAN	23

--- programmers/2020_11/day07.java
+++ programmers/2020_11/day07.java
@@ -2,22 +2,14 @@ class Solution {
     public int solution(String name) {
         int answer = 0;
 
-        // 이동횟수
+        // 위아래이동횟수
         int number = 0;
-        int right = 0; // 오른쪽 이동
-        int left = 0; // 왼쪽 이동
+        int move = name.length()-1; // 최소 좌우 이동횟수
 
         // 이름 길이 만큼 반복.
         for (int i = 0; i < name.length(); i++) {
             int getAbc = (int) name.charAt(i);
             if (65 < getAbc) {
-                if (left != 0) {
-                    right = left + right;
-                    left = 0;
-                }
-            	if (i != 0) {
-            		right++;
-            	}
             	int up = getAbc - 65; // 커서 위
             	int down = 91 - getAbc; // 커서 아래
             	if (up <= down) {
@@ -25,18 +17,21 @@ class Solution {
             	} else {
             		number += down;
             	}
-            } else if (65 == getAbc) {
-                left = right;
-//                 if (i == name.length()-1) {
-//                     rigth++;
-//                 }
-
             }
-            // System.out.print(right +":"+left);
+            
+            int next = i+1; // 현재 i위치의 다음 위치의 값을 선정한다.
+            
+            while(next<name.length() && name.charAt(next) =='A') {
+                next++;
+                // 현재 위치 다음부터 name의 길이만큼 반복하며, A의 갯수를 체크한다.
+            }
+            
+            move = Math.min(move, i+ i + (name.length() - next));
+            // 최소값을 정하는 방법 : 현재의 최소의 값과, 현재위치i에서 + 다시 처음의 위치로 돌아가서 i  + (전체에서 a의 갯수를 뺀값) 을 더한다.
+            
         }
-        // System.out.print(number);
-        answer = number + right;
+        number += move;
 
-        return answer;
+        return answer = number;
     }
 }
