c997052f6cb6d4a1a1a67386251e19368ae4d09c
YJK <70570855+ideayjk0618@users.noreply.github.com>
2020-11-10 16:19:58 +0900

    구명보트 (2차)
    정확성 에러 없음. 효율성이 전체 시간 초과.

--- programmers/2020_11/day10.java
+++ programmers/2020_11/day10.java
@@ -7,31 +7,39 @@ class Solution {
         // 오름차순
         Arrays.sort(people);
         
-        for (int i = 0; i < people.length; i++) {
-            int bef = people[i];
-            int next = people.length - 1;
-            while (next > 0) {
-                int aft = people[next];
-                if (aft == 0) {
-                    next--;
-                } else {
-                    int sum = bef + aft;
-                    if (sum > limit) {
-                        next--;
-                    } else if (sum <= limit) {
-                        answer++;
-                        people[i] = 0;
-                        people[next] = 0;
-                        flag = true;
-                        break;
-                    }
-                }
-            }
-            if (!flag) {
-                answer++;
-            }
+        // 효율성을 위한, 사람이 1명인경우 종료.
+        if (people.length == 1) {
+            return answer = 1;
         }
         
+        for (int i = 0; i < people.length; i++) {
+            flag = false;
+            int bef = people[i];
+            // System.out.print(bef);
+            if (bef != 0) {
+                int next = people.length - 1;
+                while (next > 0) {
+                    int aft = people[next];
+                    if (aft == 0) {
+                        next--;
+                    } else {
+                        int sum = bef + aft;
+                        if (sum > limit) {
+                            next--;
+                        } else if (sum <= limit) {
+                            answer++;
+                            people[i] = 0;
+                            people[next] = 0;
+                            flag = true;
+                            break;
+                        }
+                    }
+                }
+                if (!flag) {
+                    answer++;
+                }
+            }
+        }
         return answer;
     }
 }
