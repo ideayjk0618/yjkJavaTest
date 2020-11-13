770e8ae89593ba4060220ead4bd61a88e63534f5
YJK <70570855+ideayjk0618@users.noreply.github.com>
2020-11-13 22:45:08 +0900

    SUM, MAX, MIN
    ANIMAL_INS 테이블은 동물 보호소에 들어온 동물의 정보를 담은 테이블입니다. ANIMAL_INS 테이블 구조는 다음과 같으며, ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE는 각각 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부를 나타냅니다.

--- programmers/2020_11/day13.sql
+++ programmers/2020_11/day13.sql
@@ -7,3 +7,32 @@ FROM
     (SELECT DATETIME FROM ANIMAL_INS A ORDER BY DATETIME DESC) B
 WHERE
     ROWNUM = '1';
+
+-- 최대값의 문제를 구하는 방법인데 위의 방법은, 이중 select문 (서브쿼리)을 사용하여 출력하는방법이기 때문에 max의 값을 찾아 반환하는 sql을 작성해본다.
+-- 아래의 방법은 원하는 항목에 max를 붙여서 해당 열에 최고값을 출력한다.
+
+SELECT
+    MAX(DATETIME) AS "시간"
+FROM
+    ANIMAL_INS;
+
+
+-- 동물 보호소에 가장 먼저 들어온 동물은 언제 들어왔는지 조회하는 SQL 문을 작성해주세요.
+SELECT
+    MIN(DATETIME) AS "시간"
+FROM
+    ANIMAL_INS;
+
+
+-- 동물 보호소에 동물이 몇 마리 들어왔는지 조회하는 SQL 문을 작성해주세요.
+SELECT
+    COUNT(ANIMAL_ID) AS "count"
+FROM
+    ANIMAL_INS;
+    
+-- 동물 보호소에 들어온 동물의 이름은 몇 개인지 조회하는 SQL 문을 작성해주세요.
+-- 이때 이름이 NULL인 경우는 집계하지 않으며 중복되는 이름은 하나로 칩니다.
+SELECT
+    COUNT(NAME) AS "count"
+FROM
+    (SELECT DISTINCT NAME FROM ANIMAL_INS B WHERE NAME IS NOT NULL) A;
