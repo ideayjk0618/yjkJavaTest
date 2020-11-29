5bd9b2bb37b32889d9b6e7ba719d83f0bc25a4ab
YJK <70570855+ideayjk0618@users.noreply.github.com>
2020-11-29 20:38:26 +0900

    다음 큰 숫자 (효율성OK)
    <변경한 부분>
    1. split을 이용하여 배열로 문자열을 1과 비교했다 -> charAt으로 char형으로 변환하여 '1'과 비교하였다.
    2. while의 조건을 count1과 count2이 같지 않는 경우 -> n보다 nextNum이 큰경우
    3. count1과 count2가 같아지는 경우 break로 반복문을 탈출 -> 같아지는 경우 return으로 값을 넘겨주고 서비스 종료
    
    문제 설명
    자연수 n이 주어졌을 때, n의 다음 큰 숫자는 다음과 같이 정의 합니다.
    
    조건 1. n의 다음 큰 숫자는 n보다 큰 자연수 입니다.
    조건 2. n의 다음 큰 숫자와 n은 2진수로 변환했을 때 1의 갯수가 같습니다.
    조건 3. n의 다음 큰 숫자는 조건 1, 2를 만족하는 수 중 가장 작은 수 입니다.
    예를 들어서 78(1001110)의 다음 큰 숫자는 83(1010011)입니다.
    
    자연수 n이 매개변수로 주어질 때, n의 다음 큰 숫자를 return 하는 solution 함수를 완성해주세요.
    
    제한 사항
    n은 1,000,000 이하의 자연수 입니다.
    입출력 예
    n	result
    78	83
    15	23
    입출력 예 설명
    입출력 예#1
    문제 예시와 같습니다.
    입출력 예#2
    15(1111)의 다음 큰 숫자는 23(10111)입니다.

---
+++ programmers/2020_11/day29_2.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        // 제한사항
+        if (1000000 < n) {
+            return answer;
+        } else {
+            // 2진수 변환
+            String str_bi1 = Integer.toBinaryString(n);
+            // 1의 갯수
+            int count1 = 0;
+            // 반복문1: 현재 n(2)이 1이 몇개있는지 찾는다.
+            for (int i = 0; i < str_bi1.length(); i++) {
+                if ('1' == str_bi1.charAt(i)) {
+                    count1++;
+                }
+            }
+            
+            // 다음 숫자
+            int nextNum = n + 1;
+            // 1의 갯수
+            int count2 = 0;
+            // 반복문2: 조건1이 n보다 크다고했으니 다음숫자 > n로 조건을 둔다.
+            while (nextNum > n) {
+                // 다음 숫자를 2진수로 변경
+                String str_bi2 = Integer.toBinaryString(nextNum);
+                // 반복문3: n+1(2)의 1의 갯수를 센다.
+                for (int j = 0; j < str_bi2.length(); j++) {
+                    if ('1' == str_bi2.charAt(j)) {
+                        count2++;
+                    }
+                }
+                // 1의 갯수가 같으면 서비스를 종료한다.
+                if (count1 == count2) {
+                    return nextNum;
+                }
+                nextNum++;
+                count2 = 0;
+            }
+        }
+        return answer;
+    }
+}
