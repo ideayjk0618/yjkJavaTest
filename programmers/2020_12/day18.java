083e66c7e54f2f6e173752eba19469f83d9a82c6
YJK <70570855+ideayjk0618@users.noreply.github.com>
2020-12-18 20:38:58 +0900

    가장 큰 정사각형 찾기 도움말
    도움받은 고마운 곳
    https://do-rang.tistory.com/9
    https://soobarkbar.tistory.com/164
    
    문제 설명
    1와 0로 채워진 표(board)가 있습니다. 표 1칸은 1 x 1 의 정사각형으로 이루어져 있습니다. 표에서 1로 이루어진 가장 큰 정사각형을 찾아 넓이를 return 하는 solution 함수를 완성해 주세요. (단, 정사각형이란 축에 평행한 정사각형을 말합니다.)
    
    예를 들어
    
    1	2	3	4
    0	1	1	1
    1	1	1	1
    1	1	1	1
    0	0	1	0
    가 있다면 가장 큰 정사각형은
    
    1	2	3	4
    0	1	1	1
    1	1	1	1
    1	1	1	1
    0	0	1	0
    가 되며 넓이는 9가 되므로 9를 반환해 주면 됩니다.
    
    제한사항
    표(board)는 2차원 배열로 주어집니다.
    표(board)의 행(row)의 크기 : 1,000 이하의 자연수
    표(board)의 열(column)의 크기 : 1,000 이하의 자연수
    표(board)의 값은 1또는 0으로만 이루어져 있습니다.
    입출력 예
    board	answer
    [[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]	9
    [[0,0,1,1],[1,1,1,1]]	4
    입출력 예 설명
    입출력 예 #1
    위의 예시와 같습니다.
    
    입출력 예 #2
    | 0 | 0 | 1 | 1 |
    | 1 | 1 | 1 | 1 |
    로 가장 큰 정사각형의 넓이는 4가 되므로 4를 return합니다.

---
+++ programmers/2020_12/day18.java
@@ -0,0 +1,40 @@
+class Solution
+{
+    public int solution(int [][]board)
+    {
+        int answer = 0;
+        
+        // 힌트 : 동적계획법(Dynamic Programming)에서 Dottom-Up DP를 이용
+        // Bottom-Up: 작은 부분문제(Sub Problem)를 미리 계산해두고, 이 문제들을 모아 큰 문제를 해결하는 방식
+        // Top-Down: 큰 문제(Main Problem)에서 작은 부분 문제(Sub Problem)를 재귀적으로 호출하여 리턴된 값으로 문제를 해결하는 방식
+        
+        // row, column 사이즈
+        int row = board.length;
+        int column = board[0].length;
+        
+        // 1의 사이즈인경우에는 1로 리턴한다
+        if (1 == row || 1 == column) {
+            return answer = 1;
+        }
+        
+        // 사각형의 최소 단위 (1x1제외) 2x2 라고 한다면
+        // 가로세로의 길이를 알수있는 위치는 1,1(i,j)이며, 시작위치는 i,j=1부터 시작한다
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < column; j++) {
+                // 0이 아닐때 i,j의 가로세로의 길이를 알수있는 조건이 성립됨
+                if (0 != board[i][j]) {
+                    // i,j의 길이를 알기위해서는i,j의 대각선: 0,0[i-1][j-1] /위: 0,1[i-1][j] /옆: 1,0[i][j-1] 에서
+                    // 최소값을 찾아, 최소값 + 1로 i,j의 값을 갱신한다
+                    int min1 = Math.min(board[i-1][j-1], board[i-1][j]);
+                    board[i][j] = (Math.min(min1, board[i][j-1])) + 1;
+                    // 그리고 최대값을 찾는다
+                    answer = Math.max(answer, board[i][j]);
+                }
+            }
+        }
+        // 정사각형이므로 가로와 세로가 같으므로 answer을 다시 곱한다
+        answer *= answer;
+
+        return answer;
+    }
+}
