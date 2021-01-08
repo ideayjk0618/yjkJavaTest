747811992042cbb80f0a4c1ca74dc5f994f48f22
YJK <70570855+ideayjk0618@users.noreply.github.com>
2021-01-08 12:26:20 +0900

    멀쩡한 사각형
    문제 설명
    가로 길이가 Wcm, 세로 길이가 Hcm인 직사각형 종이가 있습니다. 종이에는 가로, 세로 방향과 평행하게 격자 형태로 선이 그어져 있으며, 모든 격자칸은 1cm x 1cm 크기입니다. 이 종이를 격자 선을 따라 1cm × 1cm의 정사각형으로 잘라 사용할 예정이었는데, 누군가가 이 종이를 대각선 꼭지점 2개를 잇는 방향으로 잘라 놓았습니다. 그러므로 현재 직사각형 종이는 크기가 같은 직각삼각형 2개로 나누어진 상태입니다. 새로운 종이를 구할 수 없는 상태이기 때문에, 이 종이에서 원래 종이의 가로, 세로 방향과 평행하게 1cm × 1cm로 잘라 사용할 수 있는 만큼만 사용하기로 하였습니다.
    가로의 길이 W와 세로의 길이 H가 주어질 때, 사용할 수 있는 정사각형의 개수를 구하는 solution 함수를 완성해 주세요.
    
    제한사항
    W, H : 1억 이하의 자연수
    입출력 예
    W	H	result
    8	12	80
    입출력 예 설명
    입출력 예 #1
    가로가 8, 세로가 12인 직사각형을 대각선 방향으로 자르면 총 16개 정사각형을 사용할 수 없게 됩니다. 원래 직사각형에서는 96개의 정사각형을 만들 수 있었으므로, 96 - 16 = 80 을 반환합니다.

--- programmers/2021_01/day08.java
+++ programmers/2021_01/day08.java
@@ -10,17 +10,27 @@ class Solution {
             Ox
             여기서 선으로 나눠 삼각형기준으로, 잘려지는것은 X이고, 그중에서 겹치는 사각형이 소문자x임.
             얘를 뺌, 이 2x3사이즈의 사각형 기준으로는 x가 2개고 둘이 잘린게 사각형 사이즈 한개와 같아서, 1을 뺌. 이때 이 1이 가로 세로의 최대공약수와 같다.
-            4x6인경우에는? 그려보면 선에 걸치는 소문자x가 4개고 사각형사이즈로 따지면 2개다.
+            4x6인경우에는? 그려보면 선에 걸치는 X중에 소문자x가 4개고 사각형사이즈로 따지면 2개다.
             이때 2는 4,6의 최대공약수다.
         **/
         
-         int cutting = w + h - euclidean(w, h);
+        long cutting = (long)w + (long)h - euclidean((long)w, (long)h);
+        
+        long wh = (long)w * (long)h;
+        
+        answer = wh - cutting;
         
         return answer;
+        // 후기 : 이럴거면 그냥 처음부터 w,h를 long으로 받던가, 캐스팅이 제대로 안되면 12~17번까지 실패로 뜸.
     }
     
-    // 최대공약수 구하는 유클리드 호제법 이용 (10/21문제참고)
-    public int euclidean(int w2, int h2) {
-        return 1;
+    // 최대공약수 구하는 유클리드 호제법 이용 (문제번호:12940)
+    public long euclidean(long w2, long h2) {
+        while (h2 > 0) {
+            long temp = h2;
+            h2 = w2 % h2;
+            w2 = temp;
+        }
+        return w2;
     }
 }
