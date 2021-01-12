e7e06aca8e35449b184a0d9a30d952fdea239f8a
YJK <70570855+ideayjk0618@users.noreply.github.com>
2021-01-12 12:28:13 +0900

    최댓값과 최솟값
    알고리즘스럽지 않은 풀이, 그러나 Collections을 이용해서 문제를 풀었다는점. 자주 사용하지 않는 것을 이용해서 공부했다는 점에 의의.
    다음 복습에서는 최대 최소 값을 찾는 알고리즘을 직접 이용해서 풀어볼것.

--- programmers/2021_01/day12.java
+++ programmers/2021_01/day12.java
@@ -3,17 +3,18 @@ import java.util.Collections;
 
 class Solution {
     
+    // 공백 (스페이스바)
     private static String SPACE = " ";
     
     public String solution(String s) {
         String answer = "";
-        
+        // List 생성 (숫자)
         ArrayList<Integer> sList = new ArrayList<>();
-        
+        // 반복문: 공백기준으로 문자열을 잘라서, 숫자로 캐스팅 후 List에 추가
         for (String sStr : s.split(SPACE)) {
         	sList.add(Integer.parseInt(sStr));
         }
-        
+        // Collections을 이용한 최댓값과 최솟값 찾기
         int max = Collections.max(sList);
         int min = Collections.min(sList);
         
