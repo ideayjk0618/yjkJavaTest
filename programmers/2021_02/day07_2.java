0c90e2ac9804d121de5fa83568acd674bfffcbab
YJK <70570855+ideayjk0618@users.noreply.github.com>
2021-02-07 23:29:19 +0900

    Update day07_2.java

--- programmers/2021_02/day07_2.java
+++ programmers/2021_02/day07_2.java
@@ -4,30 +4,37 @@ class Solution {
         for (int i = 1; i <= n; i++) {
             boxs += i;
         }
-        int[][] array = int new[n+1][n+1];
+        int[][] arr = new int [n+1][n+1];
         int[] answer = new int[boxs];
         int count = 1;
         int a1 = 0;
-        while (count < boxs) {
-            
-            for (int a = 0; a < n; a++) {
-                array[a1][a] = count;
+        int a = 0, b = 0, c = 0;
+        while (count <= boxs) {
+            for (a = 0; a < n; a++) {
+                arr[a1][a] = count;
                 count++;
             }
             a--;
-            for (int b = 1; b < n; b++) {
+            for (b = 1; b < n; b++) {
                 arr[a][b] = count;
                 count++;
             }
             b--;
-            for (int c = b; c > 0; c--) {
+            for (c = b; c > 0; c--) {
                 arr[c][c] = count;
                 count++;
             }
             a1++;
         }
         
-        for (int x = 0; )
+        for (int x = 0; x < arr.length; x++) {
+            for (int y = 0; y < arr[x].length; y++) {
+                int num = arr[x][y];
+                if (0 != num) {
+                    System.out.println(num);
+                }
+            }
+        }
         
         return answer;
     }
